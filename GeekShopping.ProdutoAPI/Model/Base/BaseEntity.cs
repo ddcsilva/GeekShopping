0c016fff6dc03bd83a39b9148a42a0c0feda0d77
Danilo Silva <danilo.silva@msn.com>
2024-07-05 12:33:44 -0300

    Configurando o Acesso do Microsserviço Produto.API ao Banco de Dados

---
+++ GeekShopping.ProdutoAPI/Model/Base/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShopping.ProdutoAPI.Model.Base;
+
+public class BaseEntity
+{
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+}
---
+++ GeekShopping.ProdutoAPI/Model/Context/MySQLContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProdutoAPI.Model.Context;
+
+public class MySQLContext : DbContext
+{
+    public MySQLContext() { }
+
+    public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) { }
+}
